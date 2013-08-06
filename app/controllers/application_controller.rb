8ae4e47c38bb5d5da8f199608906a42682790fff
Mike Chau <mchau@iwu.edu>
2013-08-05 21:24:55 -0500

    config devise and cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from CanCan::AccessDenied do |exception|
+    if Rails.env != "production"
+      redirect_to root_path, :alert => exception.message
+    else
+      redirect_to root_path
+    end
+  end
 end
