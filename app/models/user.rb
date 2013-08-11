03574ea5e93cdf93bb7e4459d90ff739cc9e729e
Mike Chau <mchau@iwu.edu>
2013-08-10 22:40:02 -0500

    Enable user email confirmations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,16 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :registerable, :confirmable
 
 
-  attr_accessible :role_ids, :name, :email, :password, :password_confirmation, :tier_id, :daily_limit, :as => :admin
+  attr_accessible :role_ids, :name, :email, :password, 
+                  :password_confirmation, :tier_id, 
+                  :daily_limit, :as => :admin
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, 
+                  :remember_me
   # attr_accessible :title, :body
 
   validates_presence_of :name
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130806051909) do
+ActiveRecord::Schema.define(:version => 20130811013608) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -45,8 +45,13 @@ ActiveRecord::Schema.define(:version => 20130806051909) do
     t.string   "name"
     t.integer  "tier_id"
     t.integer  "daily_limit"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
